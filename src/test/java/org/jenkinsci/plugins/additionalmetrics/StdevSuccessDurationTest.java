8f7bafe49516cf9f2055997fe5a74d70168f2685
Chadi El Masri <chadimsr@gmail.com>
2023-06-25 12:42:35 +0200

    Remove license headers from every file

--- src/test/java/org/jenkinsci/plugins/additionalmetrics/StdevSuccessDurationTest.java
+++ src/test/java/org/jenkinsci/plugins/additionalmetrics/StdevSuccessDurationTest.java
@@ -1,40 +1,10 @@
-/*
- * MIT License
- *
- * Copyright (c) 2022 Chadi El Masri
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
-
 package org.jenkinsci.plugins.additionalmetrics;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.number.OrderingComparison.greaterThan;
-import static org.jenkinsci.plugins.additionalmetrics.PipelineDefinitions.failingDefinition;
-import static org.jenkinsci.plugins.additionalmetrics.PipelineDefinitions.sleepDefinition;
-import static org.jenkinsci.plugins.additionalmetrics.PipelineDefinitions.unstableDefinition;
-import static org.jenkinsci.plugins.additionalmetrics.UIHelpers.createAndAddListView;
-import static org.jenkinsci.plugins.additionalmetrics.UIHelpers.dataOf;
-import static org.jenkinsci.plugins.additionalmetrics.UIHelpers.getListViewCell;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
+import static org.jenkinsci.plugins.additionalmetrics.PipelineDefinitions.*;
+import static org.jenkinsci.plugins.additionalmetrics.UIHelpers.*;
+import static org.junit.Assert.*;
 
 import com.google.common.collect.ImmutableList;
 import hudson.model.ListView;
