f37d0faabcd80abd376ce22ade7c778f62858659
Chadi El Masri <chadimsr@gmail.com>
2025-04-10 23:44:51 +0200

    Remove dependency on reflections library

--- src/main/java/org/jenkinsci/plugins/additionalmetrics/Metric.java
+++ src/main/java/org/jenkinsci/plugins/additionalmetrics/Metric.java
@@ -1,3 +1,10 @@
 package org.jenkinsci.plugins.additionalmetrics;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Metric {}
--- src/test/java/org/jenkinsci/plugins/additionalmetrics/Utilities.java
+++ src/test/java/org/jenkinsci/plugins/additionalmetrics/Utilities.java
@@ -1,15 +1,13 @@
 package org.jenkinsci.plugins.additionalmetrics;
 
 import com.google.common.collect.Iterables;
+import com.google.common.reflect.ClassPath;
 import hudson.views.ListViewColumn;
+import java.io.IOException;
 import java.lang.reflect.Method;
-import java.util.Collection;
-import java.util.Set;
+import java.util.Arrays;
+import java.util.List;
 import org.jenkinsci.plugins.workflow.job.WorkflowRun;
-import org.reflections.Reflections;
-import org.reflections.scanners.Scanners;
-import org.reflections.util.ClasspathHelper;
-import org.reflections.util.ConfigurationBuilder;
 
 class Utilities {
     static void terminateWorkflowRun(WorkflowRun workflowRun) {
@@ -17,22 +15,20 @@ class Utilities {
         workflowRun.doKill();
     }
 
-    static Collection<Class<? extends ListViewColumn>> getColumns() {
-        String packagePath = BuildingRunsTest.class.getPackage().getName();
+    static List<? extends Class<?>> getColumns() throws IOException {
+        Package p = Utilities.class.getPackage();
 
-        Reflections reflections =
-                new Reflections(new ConfigurationBuilder().setUrls(ClasspathHelper.forPackage(packagePath)));
-
-        return reflections.getSubTypesOf(ListViewColumn.class);
+        return ClassPath.from(ClassLoader.getSystemClassLoader()).getAllClasses().stream()
+                .filter(cl -> cl.getPackageName().equals(p.getName()))
+                .map(ClassPath.ClassInfo::load)
+                .filter(ListViewColumn.class::isAssignableFrom)
+                .toList();
     }
 
     static Method getMetricMethod(Class<? extends ListViewColumn> clazz) {
-        Reflections reflections = new Reflections(new ConfigurationBuilder()
-                .setScanners(Scanners.MethodsAnnotated)
-                .setUrls(ClasspathHelper.forClass(clazz))
-                .filterInputsBy(s -> s.equals(clazz.getName() + ".class")));
-
-        Set<Method> methods = reflections.getMethodsAnnotatedWith(Metric.class);
+        List<Method> methods = Arrays.stream(clazz.getDeclaredMethods())
+                .filter(m -> m.isAnnotationPresent(Metric.class))
+                .toList();
 
         if (methods.isEmpty()) {
             throw new RuntimeException("Expected at least one method annotated with @Metric");
